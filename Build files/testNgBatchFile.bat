a3984c4df7cc7ad78e2f5204db5b72c7a0977424
Nikhita S <s.nikhitas@gmail.com>
2016-08-26 14:32:06 +0100

    Messg to commit batch file

--- Build files/testNgBatchFile.bat
+++ Build files/testNgBatchFile.bat
@@ -1,4 +1,4 @@
-set projectLocation=C:\Users\NAIRNX\workspace\LCProject
+set projectLocation=C:\Users\NAIRNX\workspace\test
 cd %projectLocation%
 set classpath=%projectLocation%\bin;%projectLocation%\plugins\\org.testng.eclipse_6.9.12.201607091356\lib\*
 java org.testng.TestNG %projectLocation%\testng.xml
